61453e372d190e35de54d50859a2b9400f98a7ea
venkatesh7Itsme <venkateshbathala30@gmail.com>
2025-03-25 11:46:12 +0530

    Created Terms and Conditions component

---
+++ src/Components/TermsConditions.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const TermsConditions = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+     
+      <div className="bg-gray-200 py-6 px-8">
+        <h1 className="text-3xl font-light text-gray-700">TERMS AND CONDITIONS </h1>
+        <nav className="mt-2 text-gray-500 text-sm">
+          <span>HOME</span> <span className="mx-2">/</span> <span className="text-black">TERMS AND CONDITIONS</span>
+        </nav>
+      </div>
+
+      <div className="max-w-5xl mx-auto py-12 px-6">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Introduction</h2>
+        <p className="text-gray-600 mb-6">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          </p>
+          <h3 className="text-1xl font-semibold text-gray-800 mb-4">Some Title Goes Here</h3>
+        <p className="text-gray-600 mb-6">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+         </p>
+         <h3 className="text-1xl font-semibold text-gray-800 mb-4">Some Title Should be Here</h3>
+        <p className="text-gray-600 mb-6">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+        </p>
+
+        <h3 className="text-1xl font-semibold text-gray-800 mb-4">Some Title Goes Here</h3>
+        <p className="text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        
+        <h3 className="text-1xl font-semibold text-gray-800 mb-4">Some Title Should be Here</h3>
+        <p className="text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
+         </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default TermsConditions;
