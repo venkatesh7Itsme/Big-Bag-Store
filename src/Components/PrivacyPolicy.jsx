0316e05e51fe3d301e02d677d3e366769b3854ff
venkatesh7Itsme <venkateshbathala30@gmail.com>
2025-03-25 11:45:22 +0530

    Created Privacy Policy component

---
+++ src/Components/PrivacyPolicy.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+     
+      <div className="bg-gray-200 py-6 px-8">
+        <h1 className="text-3xl font-light text-gray-700">PRIVACY POLICY</h1>
+        <nav className="mt-2 text-gray-500 text-sm">
+          <span>HOME</span> <span className="mx-2">/</span> <span className="text-black">PRIVACY POLICY</span>
+        </nav>
+      </div>
+
+      <div className="max-w-5xl mx-auto py-12 px-6">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Some Title Should Be Here</h2>
+        <p className="text-gray-600 mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        </p>
+        <p className="text-gray-600 mb-6">
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <p className="text-gray-600 mb-6">
+           Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+           Duis aute irure dolor in reprehenderit  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+           Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        </p>
+        <p className="text-gray-600">
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos  qui ratione voluptatem sequi nesciunt.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
